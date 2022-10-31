85550de3a21ef27e6946d2b5a1a751e10e69067a
xshad0xx <114771213+xshad0xx@users.noreply.github.com>
2022-10-31 17:42:04 +0100

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -1,17 +1,17 @@
 setclipboard("https://discord.io/LunarCrocs")
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+OrionLib:MakeNotification({
+    Name = "Information",
+    Content = "Check Out The Lunar AI Discord Server! The Link Has Been Copied To Your Clipboard!",
+    Time = 10
+})
+
 local count = 0
 
 repeat
     warn("Check Out The Lunar AI Discord Server! The Link Has Been Copied To Your Clipboard!")
-
-    OrionLib:MakeNotification({
-        Name = "Information",
-        Content = "Check Out The Lunar AI Discord Server! The Link Has Been Copied To Your Clipboard!",
-        Time = 5
-    })
-
     count = count + 1
 until count == 3
 
@@ -23,7 +23,7 @@ if game.PlaceId == 6516141723 then
     OrionLib:MakeNotification({
         Name = "WARNING!",
         Content = "Do NOT execute Lunar AI in the lobby as it could get you permanently banned!",
-        Time = 30
+        Time = 10
     })
 
     OrionLib:Init()
